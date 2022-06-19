f75063146365e649119e49fd0017c565d74a6e31
PiTeRoVs <piotr@gronert.dev>
2022-06-19 20:08:45 +0200

    created entry route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-// server/index.js
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import express from "express"
 
-import express from "express";
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const PORT = 7070;
+const PORT = 7070
 
-const app = express();
-
-app.get("/api", (req, res) => {
-    res.json({message: "Hello from server!" });
-});
+const app = express()
+app.use('/', express.static(`${__dirname}/interface`))
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`)
 });
