fa35a41d41827638fb30dfb80caa58afd0763a3e
PiTeRoVs <piotr@gronert.dev>
2022-06-19 19:39:14 +0200

    removed unnecessary code, moved to ES6

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 // server/index.js
 
-const express = require("express");
-const cors = require("cors")
+import express from "express";
 
 const PORT = 7070;
 
 const app = express();
-
-app.use(cors())
-app.use(express.static(__dirname));
 
 app.get("/api", (req, res) => {
     res.json({message: "Hello from server!" });
